dd318b1d0c966f91f5b0119058672fd2f727dff9
ky-yk-d <key0of7one0s5life@gmail.com>
2019-01-19 01:34:59 +0900

    remove unused imports

--- src/test/java/com/example/demo/api/SampleRestApiControllerTest.java
+++ src/test/java/com/example/demo/api/SampleRestApiControllerTest.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
